cc3a25ac21d2b12bfacf72a34f392998b4229be8
srknr <k_srkn13@yahoo.co.jp>
2019-05-30 18:34:23 +0900

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { MatButtonModule } from '@angular/material/button';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, MatButtonModule, BrowserAnimationsModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
