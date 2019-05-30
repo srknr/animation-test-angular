7657a7d72312f200830586e290232b97e0282205
srknr <k_srkn13@yahoo.co.jp>
2019-05-30 18:51:57 +0900

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { slideInLeftAnimation } from './animation';
 import { trigger, transition, style, useAnimation} from '@angular/animations';
+enum Delays {
+  Fast = '0ms',
+  Second = '200ms',
+  Third = '400ms'
+};
 
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.scss' ],
-  animations: []
+  animations: [
+        trigger('delete', [/* triggerの第一引数は任意のstring */
+      transition(':enter', [
+       useAnimation(slideInLeftAnimation, {
+         params: {  delay: Delays.Third }
+       })
+      ]),
+      transition(':leave', [
+        useAnimation(slideInLeftAnimation, {
+          params: {  delay: Delays.Fast }
+        })
+      ])
+    ])
+  ]
 })
 export class AppComponent  {
 
